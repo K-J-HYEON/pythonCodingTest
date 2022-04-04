5850cf7387391d59079bba20d8099d7c1f93debe
kjh950601@naver.com <milaju@MacBook-Pro.local>
2022-04-04 09:34:53 +0900

    pythonCodingTest

---
+++ dfsBfs/queue.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+queue = deque()
+
+queue.append()
+queue.append()
+queue.append()
+queue.append()
+queue.popleft()
+queue.append()
+queue.append()
+queue.append()
+
+
+print(queue)
+queue.reverse()
+print(queue)
---
+++ dfsBfs/stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append()
+stack.append()
+stack.append()
+stack.append()
+stack.pop()
+stack.append()
+stack.append()
+stack.pop()
+
+print(stack)
+print(stack[::-1])
