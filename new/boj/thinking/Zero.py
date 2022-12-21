072cc68ac622e218484d1ee6ca94fc66605b1535
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2022-12-21 11:26:32 +0900

    boj 구현 문제

---
+++ new/boj/thinking/Zero.py
@@ -0,0 +1,12 @@
+def solve():
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        a = int(input())
+        if a != 0:
+            arr.append(a)
+        else:
+            arr.pop()
+    print(sum(arr))
+
+solve()
