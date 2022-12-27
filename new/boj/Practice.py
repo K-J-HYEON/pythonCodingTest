afb738982ccf71e45c46850ef26735d93899d17c
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2022-12-27 12:51:21 +0900

    boj 2422 브루트포스 / 그래프 문제

--- new/boj/Practice.py
+++ new/boj/Practice.py
@@ -1,10 +1,26 @@
-import sys
-input = sys.stdin.readline
+# 그래프 이론 / 브루트 포스 알고리즘 / 조합
 
-n = int(input())
-m = list(map(int, input().split()))
 
-maxNumber = max(m)
-minNumber = min(m)
+# 조합
+from itertools import combinations
+from pickle import TRUE
 
-print(maxNumber * minNumber)
+
+n, m = map(int, input().split())
+
+ice = [[False] * n for _ in range(n)]
+for i in range(m):
+    i1, i2 = map(int, input().split())
+    ice[i1-1][i2-1] = True
+    ice[i2-1][i1-1] = True
+
+result = 0
+
+for i in combinations(range(n), 3):
+    if ice[i[0]][i[1]] or ice[i[0]][i[2]] or ice[i[1]][i[2]]:
+        continue
+    result += 1
+
+print(result)
+
+
