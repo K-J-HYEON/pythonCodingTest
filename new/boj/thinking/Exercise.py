4668cf2d7053e01f1bd9ba7cd7c2d5daa5cbcca2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-03 00:09:49 +0900

    boj 구현 문제

--- new/boj/thinking/Exercise.py
+++ new/boj/thinking/Exercise.py
@@ -1,27 +1,9 @@
-# boj 1173 운동
+N = int(input())
 
-# N, m, M, T, R
-# N = 시간 / m 초기맥박 / R => 휴식 선택 맥박이 R 만큼 감소
-# m + T <= M에만 운동 가능 ///// m - R < m => 영식이 맥박 = m
-# R = > 휴식 선택하는 경우 맥박이 R 만큼 감소
-
-import sys
-input = sys.stdin.readline
-
-N, m, M, T, R = map(int, sys.stdin.readline().split())
-minute, ex_minute = 0, 0
-pulse = m
-
-while ex_minute < N:
-    if m + T > M:
-        break
-    # 운동
-    if pulse + T <= M:
-        pulse += T
-        ex_minute += 1
+for _ in range(N):
+    num = int(input())
+    k = len(str(num)) # 주어진 숫자가 몇자리 숫자인지 확인
+    if str(num**2)[-k:] == str(num): # 제곱수를 뒤에서 sli만큼 가져와 num과 동일한지 판단
+        print("YES")
     else:
-        pulse = max(pulse - R, m)
-
-    minute += 1
-print(minute if ex_minute == N else -1)
-
+        print("NO")
