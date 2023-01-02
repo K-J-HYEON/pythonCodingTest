8323a2d43bc36a848fbed742c9902b8e87ad17c6
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-03 00:13:01 +0900

    programmers playground

--- new/programmers/Practice.py
+++ new/programmers/Practice.py
@@ -1,5 +1,12 @@
-def solution(n):
+def solution(s):
     answer = []
-    for i in range(1, n+1, 2):
-        answer.append(i)
+    word_dict = {}
+    for idx, word in enumerate(list(s)):
+        if word not in word_dict:
+            answer.append(-1)
+            word_dict[word] = idx
+            print(word_dict)
+        else:
+            answer.append(idx - word_dict[word])
+            word_dict[word] = idx
     return answer
