89f0b25263f4ce12f317ac9c9e4c6fb285a4e01e
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-09 01:34:04 +0900

    CT playground

--- new/programmers/Practice.py
+++ new/programmers/Practice.py
@@ -1,12 +1,24 @@
-def solution(s):
+def solution(lottos, win_nums):
+
     answer = []
-    word_dict = {}
-    for idx, word in enumerate(list(s)):
-        if word not in word_dict:
-            answer.append(-1)
-            word_dict[word] = idx
-            print(word_dict)
-        else:
-            answer.append(idx - word_dict[word])
-            word_dict[word] = idx
+    count = 7
+
+    for i in lottos:
+        if i == 0:
+            count -= 1
+        elif i in win_nums:
+            count -= 1
+    if count > 6:
+        answer.append(6)
+    else:
+        answer.append(count)
+        count = 7
+
+    for j in lottos:
+        if j in win_nums:
+            count -= 1
+    if count > 6:
+        answer.append(6)
+    else:
+        answer.append(count)
     return answer
