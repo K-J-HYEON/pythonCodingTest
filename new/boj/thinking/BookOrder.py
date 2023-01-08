0dbaecf5716fa15f97191d7de02fc96acf76d90b
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-08 19:40:24 +0900

    boj 구현 문제

---
+++ new/boj/thinking/BookOrder.py
@@ -0,0 +1,13 @@
+# # 현재 책이 박스에 들어가지 않으면, 3번으로 감
+# # 현재 책을 현재 박스에 넣음 / 다음 책을 손에 집고 1번으로 감
+# #
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# boxes = list(map(int, input().split()))
+# books = list(map(int, input().split()))
+#
+# print(sum(boxes) - sum(books))
+
+result = 10 == 10
+print(result)
