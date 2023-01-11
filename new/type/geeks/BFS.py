7736de5414ff94257f35ef0f0cacee544806fb54
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-11 15:50:05 +0900

    BFS 예제

--- new/type/geeks/BFS.py
+++ new/type/geeks/BFS.py
@@ -1,4 +1,6 @@
 # 트리 순회할려는 그림 => 1분 안되서 외우기(dfs / bfs)
+# https://www.nossi.dev/
+# https://www.inflearn.com/course/%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%9E%85%EB%AC%B8-%ED%8C%8C%EC%9D%B4%EC%8D%AC?inst=6603e386&utm_source=instructor&utm_medium=referral&utm_campaign=inflearn_%ED%8A%B8%EB%9E%98%ED%94%BD_promotion-link
 from collections import deque
 
 def bfs(root):
