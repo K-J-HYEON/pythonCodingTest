e5a8fa165f9f6dab2bba65a2af34c182819921f0
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-11 15:50:00 +0900

    Playground

--- new/programmers/Practice.py
+++ new/programmers/Practice.py
@@ -1,24 +1,6 @@
-def solution(lottos, win_nums):
-
-    answer = []
-    count = 7
-
-    for i in lottos:
-        if i == 0:
-            count -= 1
-        elif i in win_nums:
-            count -= 1
-    if count > 6:
-        answer.append(6)
-    else:
-        answer.append(count)
-        count = 7
-
-    for j in lottos:
-        if j in win_nums:
-            count -= 1
-    if count > 6:
-        answer.append(6)
-    else:
-        answer.append(count)
-    return answer
+import sys
+n = int(sys.stdin.readline())
+sum = 0
+for i in range(n):
+    sum += int(sys.stdin.readline())
+print(sum - (n - 1))
