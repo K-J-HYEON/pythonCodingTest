d5d84ae88ca18aacad54593d3632b748725aafd4
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-12 12:20:34 +0900

    프로그래머스 lv2

---
+++ new/programmers/lv2/OpenChat.py
@@ -0,0 +1,23 @@
+# https://latte-is-horse.tistory.com/131 참고
+
+def solution(record):
+    answer = []
+    userDB = dict()
+    actions = [] # Enter, Leave, Change
+
+    for event in record:
+        info = event.split()
+        action, userid = info[0], info[1]
+        if action in ("Enter", "Change"):
+            nickname = info[2]
+            userDB[userid] = nickname
+        actions.append((action, userid))
+
+    for actionInfo in actions:
+        action, userid = actionInfo[0], actionInfo[1]
+        if action == 'Enter':
+            answer.append(f'{userDB[userid]}님이 들어왔습니다.')
+        elif action == 'Leave':
+            answer.append(f'{userDB[userid]}님이 나갔습니다.')
+
+    return answer
