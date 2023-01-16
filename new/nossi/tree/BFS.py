b8207551e54c7734095ee9b7b0c934b7cd486b95
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-16 15:04:11 +0900

    Tree - BFS

---
+++ new/nossi/tree/BFS.py
@@ -0,0 +1,24 @@
+# Traversal 트리순회
+# BFS 외우기
+from collections import deque
+
+def bfs(root):
+    # 초기세팅
+    visited = []
+    if root is None:
+        return 0;
+    q = deque()
+    q.append(root)
+
+    while q:
+        cur_node = q.popleft()
+        visited.append(cur_node.value)
+
+        if cur_node.left:
+            q.append(cur_node.left)
+        if cur_node.right:
+            q.append(cur_node.right)
+
+    return visited
+
+    bfs(root)
