668ff5f3c83d0853249f61d1349eefcfa3db3ad3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-17 15:10:36 +0900

    시간 복잡도 설계

---
+++ new/nossi/concept/TimeComplexity.py
@@ -0,0 +1,13 @@
+# https://www.inflearn.com/course/%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%9E%85%EB%AC%B8-%ED%8C%8C%EC%9D%B4%EC%8D%AC/dashboard
+# n2, n 등으로 표기
+# 삽입정렬 n2 worst case => n best case 숫자가 역순일 때 가장 높다 / average n2
+# 코딩테스트를 위한 시간복잡도
+# 1. 시간 복잡도 이해하고 외우기
+# 2. 제한 조건 보는 법
+# 3. 다양한 접근
+# 문제 이해하기 - 접근 방법 - 코드 설계 - 코드 구현
+
+
+# TwoSum 문제 예시
+# 정수가 저장된 배열 nums가 주어졌을 때, nums의 원소중 두 숫자를 더해서 target 되면 true 아니면 false
+# 같은 원소 두번사용 할수 없음
