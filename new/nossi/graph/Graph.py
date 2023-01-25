04f965cecf30d92f6b0993f5a5e229455618f0fc
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-25 12:07:23 +0900

    그래프 문제

--- new/nossi/graph/Graph.py
+++ new/nossi/graph/Graph.py
@@ -25,3 +25,42 @@ graph = {
 }
 # 암시적 그래프
 
+
+# https://leetcode.com/problems/number-of-islands/
+# brute force
+from collections import deque
+
+visit = []
+class Solution:
+    def bfs(self, v, grid):
+        global visit
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        q = deque()
+        q.append(v)
+        visit[v[0]][v[1]] = 1
+
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                    if grid[nx][ny] == "1" and not visit[nx][ny]:
+                        visit[nx][ny] = 1
+                        q.append([nx, ny])
+
+    def numIslands(self, grid):
+        global visit
+        visit = [[0] * len(grid[0]) for _ in range(len(grid))]
+        islandCnt = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and not visit[i]:
+
+                    Solution.bfs(self, [i, j], grid)
+                    islandCnt += 1
+                    return islandCnt
+
+
