0bb116f64d710c5b30395828bfe787ee26f82c98
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-26 12:56:58 +0900

    boj 이분탐색 문제

--- new/boj/thinking/Memory.py
+++ new/boj/thinking/Memory.py
@@ -1,7 +1,25 @@
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
+import sys
+input = sys.stdin.readline
 
+def binary_search(j, r, nums1, num):
+    while j <= r:
+        mid = (j + r) // 2
 
-for i in croatia:
-    word = word.replace(i, "*")
-print(len(word))
+        if nums1[mid] == num:
+            return 1
+        elif nums1[mid] < num:
+            j = mid + 1
+        else:
+            r = mid - 1
+    return 0
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    nums1 = list(map(int, input().split()))
+    nums1.sort()
+    m = int(input())
+    nums2 = list(map(int, input().split()))
+    for num in nums2:
+        print(binary_search(0, n-1, nums1, num))
