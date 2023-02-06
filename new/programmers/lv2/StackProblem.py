42091cd7f3f9fca52c20d142f9a49259813fd63e
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-06 11:00:25 +0900

    프로그래머스 stack 문제

---
+++ new/programmers/lv2/StackProblem.py
@@ -0,0 +1,26 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+def solution(numbers):
+    stack = []
+    answer = [0] * len(numbers)
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        stack.append(i)
+
+    while stack:
+        answer[stack.pop()] = -1
+
+    return answer
+
+
+# def solution(numbers):
+#     stack = []
+#     result = [-1] * len(numbers)
+#     for i in range(len(numbers)):
+#         while stack and numbers[stack[-1]] < numbers[i]:
+#             result[stack.pop()] = numbers[i]
+#
+#         stack.append(i)
+#
+#     return result
