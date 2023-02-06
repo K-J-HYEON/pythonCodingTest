ce2a78e6eb026e38c9c6572aec47d2c53b525adc
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-06 11:09:26 +0900

    프로그래머스 트리 문제

---
+++ new/boj/tree/TreeTravel.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/9372
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(T):
+    N, M = map(int, input().split())
+
+    for i in range(M):
+        a, b = map(int, input().split())
+
+    print(N - 1)
+
