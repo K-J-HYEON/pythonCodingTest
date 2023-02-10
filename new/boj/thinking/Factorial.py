d1b55c484375a3ad037907ca7a1453a39eeb23ad
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-10 09:48:10 +0900

    boj 구현 팩토리얼 문제

--- new/boj/thinking/Factorial.py
+++ new/boj/thinking/Factorial.py
@@ -1,9 +1,18 @@
+# n = int(input())
+#
+# sum = 1
+#
+# if n > 0:
+#     for i in range(n + 1):
+#         sum *= i
+#
+# print(sum)
+
+def factorial(n):
+    result = 1
+    if n > 0:
+        result = n * factorial(n-1)
+    return result
+
 n = int(input())
-
-sum = 1
-
-if n > 0:
-    for i in range(n + 1):
-        sum *= i
-
-print(sum)
+print(factorial(n))
