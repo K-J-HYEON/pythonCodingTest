256e861fae8b72b88c18c2580a197ca99a9897e9
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-20 17:39:35 +0900

    boj 이분탐색 LIS(구현 및 DP)

---
+++ new/boj/binarysearch/LIS.py
@@ -0,0 +1,52 @@
+제# https://www.acmicpc.net/problem/12015
+
+# sol 1
+import sys
+from bisect import bisect_left
+# input = sys.stdin.readline
+#
+# n = int(input())
+# a = list(map(int, input().split()))
+# lis = []
+# ans = 0
+#
+# for num in a:
+#     if not lis:
+#         lis.append(num)
+#         ans += 1
+#         continue
+#
+#     if lis[-1] < num:
+#         lis.append(num)
+#         ans += 1
+#
+#     else:
+#         index = bisect_left(lis, num)
+#         lis[index] = num
+#
+# print(ans)
+
+# sol2
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+dp = [0]
+
+for i in range(n):
+    low = 0
+    high = len(dp) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if dp[mid] < A[i]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if low >= len(dp):
+        dp.append(A[i])
+    else:
+        dp[low] = A[i]
+
+print(len(dp) - 1)
