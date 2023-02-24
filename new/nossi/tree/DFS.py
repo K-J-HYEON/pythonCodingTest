06fe73e59b44b67008654fbf4c42b92a44ffea71
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-02-24 11:32:02 +0900

    DFS 예시

--- new/nossi/tree/DFS.py
+++ new/nossi/tree/DFS.py
@@ -1,5 +1,5 @@
-# DFS
-# 깊이 우선 탐색
+# # DFS
+# # 깊이 우선 탐색
 # def dfs(root):
 #     if root is None:
 #         return
@@ -7,12 +7,12 @@
 #     dfs(root.right)
 #
 #     dfs(root)
-# root만 나한테 주면 root가 가리키는 tree에 속한 모든노드를 접근해준다라는 뜻
-
+# # root만 나한테 주면 root가 가리키는 tree에 속한 모든노드를 접근해준다라는 뜻
+#
 # def dfs(cur_node):
 #     if cur_node is None:
 #         return
 #     dfs(cur_node.left)
-#     dfs(cur_node.right)
+#     dfs(cur_node.right)a
 #
-#     dfs(root)
+# dfs(root)
