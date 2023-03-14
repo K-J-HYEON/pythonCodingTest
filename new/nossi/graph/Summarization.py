1b8fe928a8c9407623c06870c410b6771aaabac3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-03-14 14:24:34 +0900

    tree(preorder, inorder, postorder) 예제

---
+++ new/nossi/graph/Summarization.py
@@ -0,0 +1,22 @@
+# BFS
+from collections import deque
+
+def bfs(graph, start_v):
+    visited = [start_v]
+    queue = deque(start_v)
+    while queue:
+        cur_v = queue.popleft()
+        for v in graph[cur_v]:
+            if v not in visited:
+                visited.append(v)
+                queue.append(v)
+    return visited
+
+# DFS
+visited = []
+#
+# def dfs(cur_v):
+#     visited.append(cur_v)
+#     for v in graph[cur_v]:
+#         if v not in visited:
+#             dfs(v)
