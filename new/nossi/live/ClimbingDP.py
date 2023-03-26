3d055fa456df9d46d01dba0a9c88d7ac531f08f6
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-03-26 14:01:18 +0900

    ClimbingDP

--- new/nossi/live/ClimbingDP.py
+++ new/nossi/live/ClimbingDP.py
@@ -1,33 +1,31 @@
+def climbing_stairs(n):
+    memo = {}
+    def dp(n):
+        if n == 1:
+            return 1
+
+        if n == 2:
+            return 2
+
+        if n not in memo:
+            memo[n] = dp(n-1) + dp(n-2)
+
+        return memo[n]
+
+    return dp(n)
+
 # def climbing_stairs(n):
 #     memo = {}
 #
-
-#     def dp(n):
-#         if n == 1:
-#             return 1
+#     if n == 1:
+#         return 1
 #
-#         if n == 2:
-#             return 2
+#     if n == 2:
+#         return 2
 #
-#         if n not in memo:
-#             memo[n] = dp(n-1) + dp(n-2)
+#     if n not in memo:
+#         memo[n] = climbing_stairs(n-1) + climbing_stairs(n-2)
 #
-#         return memo[n]
+#     return memo[n]
 #
-#     return dp(n)
-
-def climbing_stairs(n):
-    memo = {}
-
-    if n == 1:
-        return 1
-
-    if n == 2:
-        return 2
-
-    if n not in memo:
-        memo[n] = climbing_stairs(n-1) + climbing_stairs(n-2)
-
-    return memo[n]
-
-print(climbing_stairs[10])
+# print(climbing_stairs[10])
